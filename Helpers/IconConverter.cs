462205490c0d087ef443b32574b581ccd74b582d
Emilia Drewnowska <em.drewnowska@gmail.com>
2025-06-13 01:37:00 +0200

    poki co nie dziala

---
+++ Helpers/IconConverter.cs
@@ -0,0 +1,28 @@
+﻿using FontAwesome.WPF;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kasyno.Helpers
+{
+    public class IconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string iconName)
+            {
+                if (Enum.TryParse(typeof(FontAwesomeIcon), iconName, true, out var iconObj) && iconObj is FontAwesomeIcon icon)
+                {
+                    return icon;
+                }
+                return FontAwesomeIcon.QuestionCircle;
+            }
+            return FontAwesomeIcon.QuestionCircle;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/SlotMachineViewModel.cs
+++ ViewModels/SlotMachineViewModel.cs
@@ -1,22 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Kasyno.Helpers;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using Kasyno.Views; 
-using Kasyno.Helpers;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Kasyno.ViewModels
 {
-    internal class SlotMachineViewModel
+    internal class SlotMachineViewModel : INotifyPropertyChanged
     {
+        private static readonly string[] _availableIcons =
+        {
+            "Heart", "Star", "WebAwesome", "Bomb", "Ghost", "Lemon", "Snowflake", "Fish", "UserSecret"
+        };
+
+        private readonly Random _random = new();
+
+        private string _icon1 = "QuestionCircle";
+        private string _icon2 = "QuestionCircle";
+        private string _icon3 = "QuestionCircle";
+
+        public string Icon1
+        {
+            get => _icon1;
+            set { _icon1 = value; OnPropertyChanged(); }
+        }
+
+        public string Icon2
+        {
+            get => _icon2;
+            set { _icon2 = value; OnPropertyChanged(); }
+        }
+
+        public string Icon3
+        {
+            get => _icon3;
+            set { _icon3 = value; OnPropertyChanged(); }
+        }
+
+        public ICommand NewGameCommand { get; }
         public ICommand ExitCommand { get; }
 
         public SlotMachineViewModel()
         {
+            NewGameCommand = new RelayCommand(_ => StartNewGame());
             ExitCommand = new RelayCommand(ExecuteExit);
+        }
+
+        private async void StartNewGame()
+        {
+            // Na początek ustaw znaki zapytania
+            Icon1 = "QuestionCircle";
+            Icon2 = "QuestionCircle";
+            Icon3 = "QuestionCircle";
+
+            // Małe opóźnienie, aby UI się odświeżył przed losowaniem
+            await Task.Delay(300);
+
+            Icon1 = GetRandomIcon();
+            await Task.Delay(400);
+
+            Icon2 = GetRandomIcon();
+            await Task.Delay(400);
+
+            Icon3 = GetRandomIcon();
+        }
+
+        private string GetRandomIcon()
+        {
+            int index = _random.Next(_availableIcons.Length);
+            return _availableIcons[index];
         }
 
         private void ExecuteExit(object? obj)
@@ -25,12 +79,16 @@ namespace Kasyno.ViewModels
             {
                 if (window is Window current && current.Title == "Automaty")
                 {
-                    var mainMenu = new MainMenuView(); // <-- dostosuj jeśli nazywa się inaczej
+                    var mainMenu = new Views.MainMenuView();
                     mainMenu.Show();
                     current.Close();
                     break;
                 }
             }
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }

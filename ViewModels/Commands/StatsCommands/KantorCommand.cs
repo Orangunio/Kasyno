055870393f21c0441154a6ec44b0ea7352a4e9d5
Orangunio <miloszdubowski@wp.pl>
2025-06-16 12:59:12 +0200

    Api do walut

---
+++ ViewModels/Commands/StatsCommands/KantorCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kasyno.ViewModels.Commands.StatsCommands
+{
+    public class KantorCommand : ICommand
+    {
+        private readonly StatsViewModel viewModel;
+
+        public KantorCommand(StatsViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            viewModel.OpenKantorDialog();
+        }
+    }
+
+}
